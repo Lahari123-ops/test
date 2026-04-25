9f20b0d8a7b2ed31820de3542a780e5ff3624db6
Lahari Kadirimangalam <klahari373@gmail.com>
2026-04-25 07:07:49 +0000

    filtering concept

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,72 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("basic.controller.View1", {
-        onInit: function() {
+
+        onInit: function () {
+        },
+
+        onFilterChange: function () {
+
+            var oView = this.getView();
+
             
+            var sCountry = oView.byId("countryBox").getValue();
+            var sMin = oView.byId("freightMin").getValue();
+            var sMax = oView.byId("freightMax").getValue();
+            var dFrom = oView.byId("dateFrom").getDateValue();
+            var dTo = oView.byId("dateTo").getDateValue();
+
+            var aFilters = [];
+
+            
+            if (sCountry) {
+                aFilters.push(new Filter(
+                    "ShipCountry",
+                    FilterOperator.EQ,
+                    sCountry
+                ));
+            }
+
+if (sMin && sMax) {
+    aFilters.push(new Filter(
+        "Freight",
+        FilterOperator.BT,
+        parseFloat(sMin),
+        parseFloat(sMax)
+    ));
+} else if (sMin) {
+    aFilters.push(new Filter(
+        "Freight",
+        FilterOperator.GE,
+        parseFloat(sMin)
+    ));
+} else if (sMax) {
+    aFilters.push(new Filter(
+        "Freight",
+        FilterOperator.LE,
+        parseFloat(sMax)
+    ));
+}
+            
+            if (dFrom && dTo) {
+                aFilters.push(new Filter(
+                    "OrderDate",
+                    FilterOperator.BT,
+                    dFrom,
+                    dTo
+                ));
+            }
+
+            
+            var oTable = this.byId("tableId");
+            var oBinding = oTable.getBinding("items");
+            oBinding.filter(aFilters);
         }
+
     });
 });
