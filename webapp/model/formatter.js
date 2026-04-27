0cd9e2495da0990455062315c4c49f46534ad170
Lahari Kadirimangalam <klahari373@gmail.com>
2026-04-27 08:36:03 +0000

    formatter

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -14,6 +14,57 @@ sap.ui.define([
             });
 
             return oDateFormat.format(oDate);
-        }
+        },
+         formatFreightText: function (fValue) {
+            if (fValue > 100) {
+                return "High";
+            } else if (fValue >= 50) {
+                return "Medium";
+            } else {
+                return "Low";
+            }
+        },
+
+        formatFreightState: function (fValue) {
+            if (fValue > 100) {
+                return "Error";
+            } else if (fValue >= 50) {
+                return "Warning";
+            } else {
+                return "Success";
+            }
+        },
+
+       formatDeliveryText: function (dRequired, dOrder) {
+    if (!dRequired || !dOrder) {
+        return "";
+    }
+
+    var diffDays = (dRequired - dOrder) / (1000 * 60 * 60 * 24);
+
+    if (diffDays < 5) {
+        return "Delayed";
+    } else if (diffDays <= 10) {
+        return "At Risk";
+    } else {
+        return "On Time";
+    }
+},
+
+formatDeliveryState: function (dRequired, dOrder) {
+    if (!dRequired || !dOrder) {
+        return "None";
+    }
+
+    var diffDays = (dRequired - dOrder) / (1000 * 60 * 60 * 24);
+
+    if (diffDays < 5) {
+        return "Error";
+    } else if (diffDays <= 10) {
+        return "Warning";
+    } else {
+        return "Success";
+    }
+}
     };
 });
